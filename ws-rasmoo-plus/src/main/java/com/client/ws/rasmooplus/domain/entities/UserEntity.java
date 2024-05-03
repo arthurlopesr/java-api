16346e376fbbee653fe4e4da7fc3394148f6a719
Arthur Lopes <arthurlopr12@gmail.com>
2024-05-03 14:55:49 -0300

    refactor: change the getters and setter to use lombok

--- ws-rasmoo-plus/src/main/java/com/client/ws/rasmooplus/domain/entities/UserEntity.java
+++ ws-rasmoo-plus/src/main/java/com/client/ws/rasmooplus/domain/entities/UserEntity.java
@@ -1,13 +1,22 @@
 package com.client.ws.rasmooplus.domain.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "users")
 @Entity()
 public class UserEntity implements Serializable {
+
     @Id()
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "users_id")
@@ -34,91 +43,4 @@ public class UserEntity implements Serializable {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "subscriptions_type_id")
     private SubscriptionsTypeEntity subscriptionsType;
-
-    public UserEntity(Long userId, String name, String email, String phone, String cpf, LocalDate dtSubscripton, LocalDate dtExpiration, UserTypeEntity userType, SubscriptionsTypeEntity subscriptionsType) {
-        this.userId = userId;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.cpf = cpf;
-        this.dtSubscripton = dtSubscripton;
-        this.dtExpiration = dtExpiration;
-        this.userType = userType;
-        this.subscriptionsType = subscriptionsType;
-    }
-
-    public UserEntity() {
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public LocalDate getDtSubscripton() {
-        return dtSubscripton;
-    }
-
-    public void setDtSubscripton(LocalDate dtSubscripton) {
-        this.dtSubscripton = dtSubscripton;
-    }
-
-    public LocalDate getDtExpiration() {
-        return dtExpiration;
-    }
-
-    public void setDtExpiration(LocalDate dtExpiration) {
-        this.dtExpiration = dtExpiration;
-    }
-
-    public UserTypeEntity getUserType() {
-        return userType;
-    }
-
-    public void setUserType(UserTypeEntity userType) {
-        this.userType = userType;
-    }
-
-    public SubscriptionsTypeEntity getSubscriptionsType() {
-        return subscriptionsType;
-    }
-
-    public void setSubscriptionsType(SubscriptionsTypeEntity subscriptionsType) {
-        this.subscriptionsType = subscriptionsType;
-    }
 }
--- ws-rasmoo-plus/src/main/java/com/client/ws/rasmooplus/domain/entities/UserTypeEntity.java
+++ ws-rasmoo-plus/src/main/java/com/client/ws/rasmooplus/domain/entities/UserTypeEntity.java
@@ -1,9 +1,17 @@
 package com.client.ws.rasmooplus.domain.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "user_type")
 @Entity()
 public class UserTypeEntity implements Serializable {
@@ -15,37 +23,4 @@ public class UserTypeEntity implements Serializable {
     private String name;
 
     private String description;
-
-    public UserTypeEntity(Long userId, String name, String description) {
-        this.userId = userId;
-        this.name = name;
-        this.description = description;
-    }
-
-    public UserTypeEntity() {
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescriptiom(String description) {
-        this.description = description;
-    }
 }
