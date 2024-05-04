932abe4ca5f5ce60dff131f54e370661ab2220c3
Arthur Lopes <arthurlopr12@gmail.com>
2024-05-04 14:29:05 -0300

    refactor: ensure SubscritionTypeController returns the standardized error in json format

--- ws-rasmoo-plus/src/main/java/com/client/ws/rasmooplus/domain/excepions/handler/ResourceHandler.java
+++ ws-rasmoo-plus/src/main/java/com/client/ws/rasmooplus/domain/excepions/handler/ResourceHandler.java
@@ -2,6 +2,7 @@ package com.client.ws.rasmooplus.domain.excepions.handler;
 
 import com.client.ws.rasmooplus.domain.excepions.BadRequestException;
 import com.client.ws.rasmooplus.domain.excepions.NotFoundException;
+import com.client.ws.rasmooplus.dto.error.ErrorResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,14 +12,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ResourceHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> notFoundException(NotFoundException notFoundException) {
-        String errorMessage = notFoundException.getMessage();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    public ResponseEntity<ErrorResponseDTO> notFoundException(NotFoundException notFoundException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponseDTO.builder()
+                .message(notFoundException.getMessage())
+                .httpStatus(HttpStatus.NOT_FOUND)
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .build()
+        );
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<String> badRequestException(BadRequestException badRequestException) {
-        String errorMessage = badRequestException.getMessage();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    public ResponseEntity<ErrorResponseDTO> badRequestException(BadRequestException badRequestException) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDTO.builder()
+                .message(badRequestException.getMessage())
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build()
+        );
     }
 }
