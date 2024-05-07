564fa8f907537748f5395eb56b59c2c8e24be98c
Arthur Lopes <arthurlopr12@gmail.com>
2024-05-07 19:01:36 -0300

    feat: add CustumerDTO for integration with new gateway

---
+++ ws-rasmoo-plus/src/main/java/com/client/ws/rasmooplus/infra/gateways/communication/custumer/dto/CustumerDTO.java
@@ -0,0 +1,22 @@
+package com.client.ws.rasmooplus.infra.gateways.communication.custumer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustumerDTO {
+
+    private String id;
+
+    private String email;
+
+    private String cpf;
+
+    private String fisrtName;
+
+    private String lastName;
+
+}
