b3d9ba0a5b859582c97bac67615c7ba2ff869b35
Arthur Lopes <arthurlopr12@gmail.com>
2024-05-25 18:17:19 -0300

    chore: add redis in project

--- ws-rasmoo-plus/src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
+++ ws-rasmoo-plus/src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableCaching
 @OpenAPIDefinition(
         info = @Info(title = "API de criação de usuários para um sistema de transações financeiras",
                 version = "1.0",
--- ws-rasmoo-plus/src/main/java/com/client/ws/rasmooplus/presentation/controllers/SubscriptionTypeController.java
+++ ws-rasmoo-plus/src/main/java/com/client/ws/rasmooplus/presentation/controllers/SubscriptionTypeController.java
@@ -5,6 +5,8 @@ import com.client.ws.rasmooplus.presentation.dto.SubscriptionTypeDTO;
 import com.client.ws.rasmooplus.useCases.SubscriptionTypeUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,26 +20,31 @@ public class SubscriptionTypeController {
     @Autowired
     private SubscriptionTypeUseCase subscriptionTypeUseCase;
 
+    @Cacheable(value = "subscriptionType")
     @GetMapping
     public ResponseEntity<List<SubscriptionsTypeEntity>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeUseCase.findAll());
     }
 
+    @Cacheable(value = "subscriptionType", key = "#id")
     @GetMapping("/{id}")
     public ResponseEntity<SubscriptionsTypeEntity> findById(@PathVariable("id") Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeUseCase.findById(id));
     }
 
+    @CacheEvict(value = "subscriptionType", allEntries = true)
     @PostMapping
     public ResponseEntity<SubscriptionsTypeEntity> create(@Valid @RequestBody SubscriptionTypeDTO subscriptionsTypeDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(subscriptionTypeUseCase.create(subscriptionsTypeDto));
     }
 
+    @CacheEvict(value = "subscriptionType", allEntries = true)
     @PutMapping("/{id}")
     public ResponseEntity<SubscriptionsTypeEntity> update(@PathVariable("id") Long id, @RequestBody SubscriptionTypeDTO subscriptionsTypeDto) {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeUseCase.update(id, subscriptionsTypeDto));
     }
 
+    @CacheEvict(value = "subscriptionType", allEntries = true)
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
         subscriptionTypeUseCase.delete(id);
